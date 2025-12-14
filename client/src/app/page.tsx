dfdca1c41ed9f2f3c6036f3f0c36310545fb2484
ConfusezNy <natthachai.confuse@gmail.com>
2025-12-14 14:53:39 +0700

    login show profile success

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
   return (
     
       <main className="text-9xl flex min-h-screen flex-col items-center justify-between p-24">
-        รักเมียครับ
+        รักเมียครับ <br />
+        (เมียใคร )
       </main>
      
  
